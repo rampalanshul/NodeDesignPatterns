c15c5b7280f95f72b7eb99c0c1e2660e1a55ddae
SakaitBhandari2807 <sakaitbhandari@outlook.com>
2018-10-04 22:54:44 +0530

    Added Spider.js

---
+++ Spider.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const https = require('https');
+
+
+// function spider(url,callback){
+//     const filename = getFileName(url);
+
+//     fs.exists(filename,flag=>{
+//         if(flag)
+//             return callback(null,filename,true);
+//     });
+//     request(url,(err,))
+// }
+
+
+// spider(process.argv[2],(err,filename,downloaded)=>{
+//     if(err)
+//         // do something;
+//         console.log();
+//     else if(downloaded){
+//             // again do something
+//             console.log(`Dowloading of file ${filename} is complete..`);
+//     }
+// });
+
+
+
+function showmovies(){
+    https.get('https://yts.am/api/v2/list_movies.json',(response)=>{
+        console.log(response);
+    });
+}
+
+showmovies();
+
+
+
+Client ID: 272780864268-7e9be73ugh4q2irsp1m09t2hufeto3jf.apps.googleusercontent.com
+Client Secret: ijR6WAh0P5rq3mRN4JeIK7hy
