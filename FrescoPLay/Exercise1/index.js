e5f6c85c17d0d0882c2f14bd86c02c4b55c92162
SakaitBhandari2807 <sakaitbhandari@outlook.com>
2018-10-12 18:45:17 +0530

    Added two more exercise

---
+++ FrescoPLay/Exercise1/index.js
@@ -0,0 +1,55 @@
+// var operators = [];
+// var operands = [];
+// // var html = document.getElementById('screen');
+
+// function update(value) {
+//     //Type the code here.
+//     if(value != "+" && value != "-" && value != '*' && value != "/"){
+//         document.getElementById('screen').value = value;
+//         operands.push(value);
+//     }   
+//     else{
+//         document.getElementById('screen').value = value;
+//         operators.push(value);
+//     }
+// }
+
+// function result() {
+//     //Type the code here.
+//     var a = operands.pop();
+//     var b = operands.pop();
+//     var op = operators.pop();
+//     if(op == "+")
+//         document.getElementById('screen').value = a+b;
+//     else if(op == "-")
+//         document.getElementById('screen').value = a-b;
+//     else if(op == '*')
+//         document.getElementById('screen').value = a*b;
+//     else
+//         document.getElementById('screen').value = a/b;
+// }
+
+// function reset() {
+//     //Type the code here.
+//     operators = [];
+//     operands = [];
+//     document.getElementById('screen').value = "";
+// }
+
+
+// // update(1);
+// // update("*");
+// // update(2);
+// // console.log(result());
+// // reset();
+
+(function f(){ 
+    function f(){ 
+        return 1;
+     } 
+   return f(); 
+  function f()
+   { 
+       return 2; 
+    } 
+})();
